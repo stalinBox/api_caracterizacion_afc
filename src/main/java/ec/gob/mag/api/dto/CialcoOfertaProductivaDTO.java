c01521feb0d3de06346bf4cd6940c0233979b11d
Darwin Stalin Ramirez Supe <dramirez@mag.gob.ec>
2021-09-17 09:49:22 -0500

    algo

--- src/main/java/ec/gob/mag/api/dto/CialcoOfertaProductivaDTO.java
+++ src/main/java/ec/gob/mag/api/dto/CialcoOfertaProductivaDTO.java
@@ -30,7 +30,6 @@ public class CialcoOfertaProductivaDTO {
 	private Long ciop_act_usu;
 	private Boolean ciop_eliminado;
 	private String nombre_ciop_cat_id_oferta;
-	private Integer ciop_cat_id_act_economica;
 	private Integer totalRecords;
 	private String ciop_cat_ids_ruta;
 	private String nombres_ciop_cat_ids_ruta;
